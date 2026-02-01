1ef6e03646409d1ba8ceca6a78de5b2dc7dee055
24ciq <yassirgabeer2@gmail.com>
2026-02-01 19:07:28 +0300

    Implement URL parameter reading in script.js
    Add functionality to read URL parameters and update DOM elements.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Imsakiya script loaded");
+// قراءة بيانات الرابط
+const params = new URLSearchParams(window.location.search);
+const name = params.get("name") || "—";
+const img = params.get("img") || "photos/default.jpg";
+
+// وضع البيانات
+document.getElementById("name").textContent = name;
+document.getElementById("photo").src = img;
